5306c8b7b45dfc3d264784bba8b1a018553e32d4
Recep Cetin <recep.cetin@outlook.com>
2018-03-01 21:33:07 +0100

    hello world

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -5,7 +5,7 @@ import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
-    <h1>Hello world</h1>
+    <h1>Hello World</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
     <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
